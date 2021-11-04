da631771dc6064cfb248207d0e58d659f1cbb7c0
alpaca <zxcvbnm5288@gmail.com>
2021-11-04 12:23:23 +0900

    return Negative

---
+++ Codewar/8 kyu/Return Negative/answer.js
@@ -0,0 +1,17 @@
+function makeNegative(num) {
+  return num > 0 ? -num : num;
+}
+
+/*
+examples
+
+<case 1>
+num = 42
+
+return = -42
+
+<case 2>
+num = 0
+
+return = 0
+*/
---
+++ Codewar/8 kyu/Return Negative/review.md
@@ -0,0 +1,3 @@
+### [Return Negative](https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript)
+
+너무 간단한 문제였다 `8 kyu`는 손대지 않는걸로 해야겠다
