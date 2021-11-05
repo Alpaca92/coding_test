489557a2b0e695475d6b4ffc05c9f38036dc4fd5
alpaca <zxcvbnm5288@gmail.com>
2021-11-05 15:37:42 +0900

    체육복*

--- Programmers/lv_1/체육복*/answer.js
+++ Programmers/lv_1/체육복*/answer.js
@@ -1,33 +1,22 @@
-const n = 5;
-let lost = [2, 3, 4];
-const reserve = [3, 4, 5];
-
 function removeElement(arr, idx) {
   return arr.slice(0, idx).concat(arr.slice(idx + 1));
 }
 
 function solution(n, lost, reserve) {
-  for (const student of reserve) {
-    if (lost.includes(student)) {
-      const idx = lost.indexOf(student);
-
-      lost = removeElement(lost, idx);
-      continue;
+  reserve.forEach((student) => {
+    if (lost.indexOf(student) !== -1) {
+      lost = removeElement(lost, lost.indexOf(student));
+    } else {
+      if (lost.indexOf(student - 1) !== -1) {
+        lost = removeElement(lost, lost.indexOf(student - 1));
+      } else if (lost.indexOf(student + 1) !== -1) {
+        lost = removeElement(lost, lost.indexOf(student + 1));
+      }
     }
-    if (lost.indexOf(student - 1) !== -1) {
-      const idx = lost.indexOf(student - 1);
+  });
 
-      lost = removeElement(lost, idx);
-    } else if (lost.indexOf(student + 1) !== -1) {
-      const idx = lost.indexOf(student + 1);
-
-      lost = removeElement(lost, idx);
-    }
-  }
   return n - lost.length;
 }
-
-console.log(solution(n, lost, reserve));
 
 /*
 examples
@@ -52,4 +41,17 @@ lost = [3]
 reserve = [1]
 
 return = 2
+*/
+
+/*
+pseudo code
+
+예약자의 본인의 이름이 있는지 확인한다
+있다면 예약자에서 본인의 이름을 없애고 분실자에서도 해당 번호의 이름을 지운다
+
+예약자의 전번호가 lost에 있는지를 확인한다
+있다면 예약자에서 본인의 이름을 없애고 분실자에서도 해당 번호의 이름을 지운다
+예약자의 전원을 다 훑어봤다면 이번엔 뒷번호가 lost에 있는지를 확인하고 지우기를 반복한다
+
+반복이 다 끝났다면 n - lost.length가 수업을 들을 수 있는 인원 수이다
 */
