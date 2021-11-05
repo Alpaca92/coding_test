e9c3b3feec3f8aa768a4383021896ad922f63508
alpaca <zxcvbnm5288@gmail.com>
2021-11-05 14:40:34 +0900

    가장 큰 수*

--- Programmers/lv_2/가장 큰 수*/answer.js
+++ Programmers/lv_2/가장 큰 수*/answer.js
@@ -1,12 +1,13 @@
 function solution(numbers) {
-  const MultipliersOfTen = numbers.filter((number) => !(number % 10));
-
-  console.log(MultipliersOfTen);  
+  return numbers
+    .map((number) => number.toString())
+    .sort((a, b) => b[0] - a[0])
+    .join("");
 }
 
 numbers = [3, 30, 34, 5, 9];
 
-solution(numbers);
+console.log(solution(numbers));
 
 /*
 examples
@@ -20,4 +21,10 @@ return = "6210"
 numbers = [3, 30, 34, 5, 9]
 
 return = "9534330"
+*/
+
+/*
+psuedo code
+
+// 가장 왼쪽에 있는 숫자 ex. 3000 => 3, 49819027 => 4를 기준으로 내림차순 정렬한다
 */
