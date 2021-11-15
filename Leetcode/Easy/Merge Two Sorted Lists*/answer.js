40755c7df4e5d330378fa474ff7adb4f7075dff1
alpaca <zxcvbnm5288@gmail.com>
2021-11-15 18:49:10 +0900

    Merge Two Sorted Lists*

---
+++ Leetcode/Easy/Merge Two Sorted Lists*/answer.js
@@ -0,0 +1,32 @@
+var mergeTwoLists = function (l1, l2) {};
+
+/*
+examples
+
+<case 1>
+l1 = [1,2,4]
+l2 = [1,3,4]
+
+return = [1,1,2,3,4,4]
+
+<case 2>
+l1 = []
+l2 = []
+
+return = []
+
+<case 3>
+l1 = []
+l2 = [0]
+
+return = [0]
+*/
+
+/*
+pseudo code
+
+제약 사항으로 sort등의 함수를 쓸 수 없게 되어있음
+this.val = this[0], this.next = this.slice(1)로 구성되어 있음
+
+ListNode에 대한 이해가 없어서 아직 못풀겠음
+*/
