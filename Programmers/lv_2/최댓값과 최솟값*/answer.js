4f94efce0bc9a2b03ee6f0cf91e91ae4d4af9835
alpaca <zxcvbnm5288@gmail.com>
2022-01-16 22:23:32 +0900

    최댓값과 최솟값*

---
+++ Programmers/lv_2/최댓값과 최솟값*/answer.js
@@ -0,0 +1,28 @@
+function solution(s) {
+  //
+}
+
+/*
+pseudo code
+
+
+*/
+
+/*
+examples
+
+<case 1>
+s = "1 2 3 4"
+
+return = "1 4"
+
+<case 2>
+s = "-1 -2 -3 -4"
+
+return = "-4 -1"
+
+<case 3>
+s = "-1 -1"
+
+return = "-1 -1"
+*/
