064996cd1163f5e8ef07736bbd4da3a7a507fbac
alpaca <zxcvbnm5288@gmail.com>
2022-03-22 23:29:33 +0900

    짝지어 제거하기*

--- Programmers/lv_2/짝지어 제거하기/answer.js
+++ Programmers/lv_2/짝지어 제거하기/answer.js
@@ -1,14 +1,5 @@
 function solution(s) {
-  const regex = /(\w)\1/g;
-
-  while (s.match(regex)) {
-    s = s.replace(regex, "");
-  }
-
-  return s === "" ? 1 : 0;
 }
-
-solution("baabaa");
 
 /*
 examples
