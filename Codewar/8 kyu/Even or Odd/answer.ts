00ba8efdd78c4ad25e0d0b8918a33f338a49f2dc
ayaan <zxcvbnm5288@gmail.com>
2022-08-31 01:15:53 +0900

    TS: Even or Odd

---
+++ Codewar/8 kyu/Even or Odd/answer.ts
@@ -0,0 +1,39 @@
+export function even_or_odd(n: number): string {
+  if (n % 2) return "Odd";
+
+  return "Even";
+}
+
+/*
+examples
+
+<case 1>
+n = 2
+
+result = "Even"
+
+<case 2>
+n = 0
+
+result = "Even"
+
+<case 3>
+n = 7
+
+result = "Odd"
+
+<case 4>
+n = 1
+
+result = "Odd"
+
+<case 5>
+n = -4
+
+result = "Even"
+
+<case 6>
+n = -3
+
+result = "Odd"
+*/
---
+++ Codewar/8 kyu/Even or Odd/review.md
@@ -0,0 +1,5 @@
+### [Even or Odd](https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/typescript)
+
+이제 ts를 중점적으로 공부하기 위해 낮은 레벨부터 ts로 알고리즘 문제를 풀기로 했다
+
+풀면서 차차 익숙해지면 좋을 것 같다
