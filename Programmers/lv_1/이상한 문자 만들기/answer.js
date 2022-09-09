6b0b0cb92c5be51141a526de0dd3b3a996ff1b34
ayaan <zxcvbnm5288@gmail.com>
2022-09-09 19:21:09 +0900

    이상한 문자 만들기

---
+++ Programmers/lv_1/이상한 문자 만들기/answer.js
@@ -0,0 +1,10 @@
+function solution(s) {
+  return s
+    .split(" ")
+    .map((word) =>
+      [...word]
+        .map((char, idx) => (idx % 2 ? char.toLowerCase() : char.toUpperCase()))
+        .join("")
+    )
+    .join(" ");
+}
---
+++ Programmers/lv_1/이상한 문자 만들기/review.md
@@ -0,0 +1,1 @@
+### [이상한 문자 만들기](https://school.programmers.co.kr/learn/courses/30/lessons/12930)
