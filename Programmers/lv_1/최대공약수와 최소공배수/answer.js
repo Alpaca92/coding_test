b62a5e3bcab897ae8ece203d838be61cc3bba8ab
Ayaan <zxcvbnm5288@gmail.com>
2022-09-11 17:54:08 +0900

    최대공약수와 최소공배수(new)

--- Programmers/lv_1/최대공약수와 최소공배수/answer.js
+++ Programmers/lv_1/최대공약수와 최소공배수/answer.js
@@ -1,22 +1,17 @@
+function getGcd(n, m) {
+  const largeNum = n > m ? n : m;
+  const smallNum = n > m ? m : n;
+  const remainder = largeNum % smallNum;
+
+  return remainder ? getGcd(smallNum, remainder) : smallNum;
+}
+
+function getLcm(n, m) {
+  const gcd = getGcd(n, m);
+
+  return (n * m) / gcd;
+}
+
 function solution(n, m) {
-  let min = (n > m) ? m : n;
-  let max = (n > m) ? n : m;
-  
-  let GCD = 0;
-  
-  while (min !== 0) {
-      let temp = min;
-      min = max % min;
-      max = temp;
-      
-      if (!(max % min)) {
-          GCD = max;
-      }
-  }
-  
-  let LCM = n * m / GCD;
-  
-  const answer = [GCD, LCM];
-  
-  return answer;
+  return [getGcd(n, m), getLcm(n, m)];
 }
--- Programmers/lv_1/최대공약수와 최소공배수/review.md
+++ Programmers/lv_1/최대공약수와 최소공배수/review.md
@@ -1,1 +1,14 @@
 ### [최대공약수와 최소공배수](https://school.programmers.co.kr/learn/courses/30/lessons/12940)
+
+유클리드 호제법을 사용하는 것이 가장좋은듯 하다
+
+```js
+function greatestCommonDivisor(a, b) {return b ? greatestCommonDivisor(b, a % b) : Math.abs(a);}
+function leastCommonMultipleOfTwo(a, b) {return (a * b) / greatestCommonDivisor(a, b);}
+function gcdlcm(a, b) {
+    return [greatestCommonDivisor(a, b),leastCommonMultipleOfTwo(a, b)];
+}
+```
+
+나도 깔끔하게 짰다고 생각했는데 위 사람의 코드를 보면 굉장히 깔끔하게 짠 것을 볼 수 있다
+
