7d55fee0cda2efb9bd7f8eb8e8159940c58484ca
ayaan <zxcvbnm5288@gmail.com>
2022-09-15 22:37:02 +0900

    큰 수 만들기(ing)

--- Programmers/lv_2/큰 수 만들기 !/answer.js
+++ Programmers/lv_2/큰 수 만들기 !/answer.js
@@ -1,10 +1,25 @@
 function solution(number, k) {
+  const stack = [];
+  const numberArr = number.split('');
   
+  for(let i = 0; i < numberArr.length - 1; i++) {
+    if (numberArr[i] > numberArr[i + 1]) {
+      stack.push([i, numberArr[i]]);
+    } else {
+      delete numberArr[i];
+
+      if (stack.length) {
+        // nested for loop여서 안될 것 같음
+      }
+    }
+  }
 }
 
 /*
 pseudo code
 
+1. 현재 인덱스와 다음 인덱스의 숫자를 비교하여 현재값이 더 작다면 현재값을 지운다
+2. 숫자가 지워지지 않았다면 인덱스와 숫자를 stack에 저장한다
 */
 
 /*
