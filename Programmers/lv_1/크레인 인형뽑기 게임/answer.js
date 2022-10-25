2b27c1e8b605bddfcecbbfe47fdd28bde37edb9e
Ayaan(office mac) <zxcvbnm5288@gmail.com>
2022-10-25 09:32:29 +0900

    retry: 크레인 인형뽑기 게임 !

--- Programmers/lv_1/크레인 인형뽑기 게임/answer.js
+++ Programmers/lv_1/크레인 인형뽑기 게임/answer.js
@@ -1,6 +1,33 @@
 function solution(board, moves) {
-  var answer = 0;
-  return answer;
+  return moves.reduce(
+    (result, move) => {
+      let caught = null;
+
+      for (let row = 0; row < board.length; ++row) {
+        const column = move - 1;
+
+        if (board[row][column] !== 0) {
+          caught = board[row][column];
+          board[row][column] = 0;
+          break;
+        }
+      }
+      if (!caught) {
+        return result;
+      } else if (result.stack.at(-1) === caught) {
+        ++result.counter;
+        result.stack.pop();
+
+        return result;
+      } else if (!caught && result.stack.at(-1) !== caught) {
+        return {
+          counter,
+          stack: [...result.stack, caught],
+        };
+      }
+    },
+    { counter: 0, stack: [] }
+  ).counter;
 }
 
 /*
@@ -11,13 +38,4 @@ board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
 moves = [1,5,3,5,1,2,1,4]
 
 result = 4
-*/
-
-/*
-pseudo code
-
-헷갈리지 않게 moves를 다 -1씩 빼준다(col을 idx화 시킴)
-배열의 각 col의 idx를 모두 선회했을 때 0이면 아무일도 일어나지 않음
-무언가 있다면 그 값을 0으로 치환하고 기존 값을 '바구니'에 넣는다
-바구니에 넣었을 때 이전 값과 같은 값이면 그 두개를 없애고 result += 2를 한다
 */
