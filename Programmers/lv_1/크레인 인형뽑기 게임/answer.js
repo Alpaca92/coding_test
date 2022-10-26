e8007ef9d8d23e7837de28df806de0625fc9594f
Ayaan(office mac) <zxcvbnm5288@gmail.com>
2022-10-26 18:07:08 +0900

    크레인 인형뽑기 게임

--- Programmers/lv_1/크레인 인형뽑기 게임/answer.js
+++ Programmers/lv_1/크레인 인형뽑기 게임/answer.js
@@ -1,33 +1,36 @@
 function solution(board, moves) {
-  return moves.reduce(
-    (result, move) => {
-      let caught = null;
-
-      for (let row = 0; row < board.length; ++row) {
+  return (
+    moves.reduce(
+      (result, move) => {
+        const lastElement = result.stack[result.stack.length - 1];
         const column = move - 1;
+        let caught = null;
 
-        if (board[row][column] !== 0) {
-          caught = board[row][column];
-          board[row][column] = 0;
-          break;
+        for (let row = 0; row < board.length; ++row) {
+          if (board[row][column] !== 0) {
+            caught = board[row][column];
+            board[row][column] = 0;
+            break;
+          }
         }
-      }
-      if (!caught) {
-        return result;
-      } else if (result.stack.at(-1) === caught) {
-        ++result.counter;
-        result.stack.pop();
 
-        return result;
-      } else if (!caught && result.stack.at(-1) !== caught) {
-        return {
-          counter,
-          stack: [...result.stack, caught],
-        };
-      }
-    },
-    { counter: 0, stack: [] }
-  ).counter;
+        if (!caught) {
+          return result;
+        } else if (lastElement === caught) {
+          ++result.counter;
+          result.stack.pop();
+
+          return result;
+        } else if (caught && lastElement !== caught) {
+          return {
+            counter: result.counter,
+            stack: [...result.stack, caught],
+          };
+        }
+      },
+      { counter: 0, stack: [] }
+    ).counter * 2
+  );
 }
 
 /*
